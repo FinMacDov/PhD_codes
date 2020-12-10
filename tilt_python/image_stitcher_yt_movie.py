ce0963727c835ae2ce708c5f959c559bec8095f5
FinMacDov <fmackenziedover1@sheffield.ac.uk>
2020-12-10 23:56:21 +0000

    needed new code for yt images

---
+++ tilt_python/image_stitcher_yt_movie.py
@@ -0,0 +1,73 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from pathlib import Path as Path_creator
+import glob
+
+
+def gallery(array, ncols):
+    nindex, height, width, intensity = array.shape
+    nrows = nindex//ncols
+    assert nindex == nrows*ncols
+    # want result.shape = (height*nrows, width*ncols, intensity)
+    result = (array.reshape(nrows, ncols, height, width, intensity)
+              .swapaxes(1, 2)
+              .reshape(height*nrows, width*ncols, intensity))
+    return result
+
+
+def make_array(image_name):
+    from PIL import Image
+    first = True
+    for img in image_name:
+#        print(img[0])
+        if first:
+            first = False
+            image = np.array([np.asarray(Image.open(img[0]))])
+        else:
+            image = np.vstack((image, np.array([np.asarray(Image.open(img[0]))])))
+    return image
+
+
+path_2_shared_drive = '/run/user/1001/gvfs/smb-share:server=uosfstore.shef.ac.uk,share=shared/mhd_jet1/User/smp16fm' 
+
+#name = [ 'jet_P300_B60_A60_T5', 
+#        'jet_P300_B60_A60_T10', 'jet_P300_B60_A60_T15',
+#        'jet_P300_B60_A60_T20', 'jet_P300_B60_A60_T25',
+#        'jet_P300_B60_A60_T30', 'jet_P300_B60_A60_T35',
+#        'jet_P300_B60_A60_T40', 'jet_P300_B60_A60_T45',
+#        'jet_P300_B60_A60_T50', 'jet_P300_B60_A60_T55',
+#        'jet_P300_B60_A60_T60']
+
+name = [ 'jet_P300_B60_A60_T0', 'jet_P300_B60_A60_T15', 
+        'jet_P300_B60_A60_T30', 'jet_P300_B60_A60_T45']
+tilt_nbs = [nam.split('T')[-1]  for nam in name]
+jet_name =  name[0].split('T')[0]
+tilt_nbs = '_'.join(tilt_nbs)
+jet_save_name = jet_name+'T_'+tilt_nbs
+nb_of_images_per_plot = 4
+img_root_folder = path_2_shared_drive + '/j/tilt_python/sharc_run/yt_images/'
+ti_number = np.arange(0,131)
+#ti_number = np.arange(0,3)
+save_dir = img_root_folder+'figs_for_pres/'+jet_save_name+'/'
+Path_creator(save_dir).mkdir(parents=True, exist_ok=True)
+for time in ti_number:    
+    list_of_dirs = []
+    for name_path in name:
+        dir_2_image = glob.glob(img_root_folder+name_path+'/t_'+str(time).zfill(4)+'/*.png')
+        list_of_dirs.append(dir_2_image)
+    
+    f, ax = plt.subplots()
+    f.set_size_inches(64, 36)
+    #f.set_size_inches(32, 18)
+    #makes it worse
+    #f.set_size_inches(16, 9)
+    
+    stack_of_images = make_array(list_of_dirs)
+    result = gallery(stack_of_images, nb_of_images_per_plot)
+    plt.axis('off')
+    plt.imshow(result)
+    f.savefig(save_dir+jet_save_name+'_'+str(time).zfill(4)+'.png', bbox_inches='tight')
+    f.clf()
+    plt.close()
+#plt.savefig(save_dir+name[idx]+marker+'.png', bbox_inches='tight',
+#            format='png', dpi=800, pad_inches = 0)
