622c62939f2e1599f7785e257adbbf2d65d8f083
FinMacDov <fmackenziedover1@sheffield.ac.uk>
2021-01-18 17:33:25 +0000

    new example

--- python/video_maker_for_single_images.py
+++ python/video_maker_for_single_images.py
@@ -7,10 +7,16 @@ import cv2
 #file_path = 'jet_P300_B60_A60/t*/*sch*'
 #save_dir = 'jet_P300_B60_A60/movie'
 
+#movie_name = 'den'
+#file_path = 'jet_P300_B50A_60_/t*/*den*'
+#path_2_images = glob.glob(file_path)
+#save_dir = 'jet_P300_B50A_60_/movie'
+
 movie_name = 'den'
-file_path = 'jet_P300_B50A_60_/t*/*den*'
+file_path = 'image_check_sj/jet*'
 path_2_images = glob.glob(file_path)
-save_dir = 'jet_P300_B50A_60_/movie'
+save_dir = 'image_check_sj/movie'
+
 # stupid error where 10 is first, will need to rename files
 # For these purpose skipping one time step is fine
 Path(save_dir).mkdir(parents=True, exist_ok=True)    
