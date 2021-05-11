7a7b8da3064b85c0bb287b78e93126af1c274319
FinMacDov <fmackenziedover1@sheffield.ac.uk>
2021-05-11 11:11:15 +0100

    backing up after PhD results, codes need cleaning

--- python/video_maker_for_single_images.py
+++ python/video_maker_for_single_images.py
@@ -12,11 +12,25 @@ import cv2
 #path_2_images = glob.glob(file_path)
 #save_dir = 'jet_P300_B50A_60_/movie'
 
-movie_name = 'den'
-file_path = 'image_check_sj/jet*'
-path_2_images = glob.glob(file_path)
-save_dir = 'image_check_sj/movie'
+#movie_name = 'rho_Te_sch'
+#file_path = 'jet_P300_B60A_60_/figs_for_sj_paper/rho_te_sch/rho*'
 
+#movie_name = 'sj_jet'
+#file_path = 'sj_paper/movie/rho_Te_sch/sj_image_for_movie_*.png'
+
+movie_name = 'pscan_jet'
+file_path = 'sj_paper/movie/pscan/pscan_for_movie*.png'
+
+path_2_images = glob.glob(file_path)
+path_2_images = np.asarray(path_2_images)
+list_of_inds = []
+for p_name in path_2_images:
+#    list_of_inds.append(int(p_name.split('_')[-1].split('.')[0]))
+    list_of_inds.append(int(p_name.split('_')[-1].split('.')[0].split('e')[-1]))
+fixed_indexs = np.argsort(list_of_inds)
+path_2_images = path_2_images[fixed_indexs]
+#save_dir = 'image_check_sj/movie'
+save_dir = 'sj_paper/movie'
 # stupid error where 10 is first, will need to rename files
 # For these purpose skipping one time step is fine
 Path(save_dir).mkdir(parents=True, exist_ok=True)    
